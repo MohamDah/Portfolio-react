164030472af640615462a344f4fc55feb9a46bcc
MohamDah <mohammedfaiseldhb@gmail.com>
2024-10-12 10:12:54 +0200

    changed work section

--- src/Work.jsx
+++ src/Work.jsx
@@ -7,22 +7,22 @@ import projects from "./projects"
 export default function Work() {
     const workEls = projects.map(project => (
         <a key={project.id} href={project.url} className="portfolio__item">
+                <p className="portfolio__name">{project.name}</p>
                 <img src={project.image} alt={project.name} className="portfolio__img" />
         </a>
     ))
     return (
         <section className="my-work" id="work">
             <h2 className="section__title section__title--work">My work</h2>
-            <p className="section__subtitle section__subtitle--work">A selection of my range of work</p>
+            <p className="section__subtitle section__subtitle--work">A selection of my deployed React projects</p>
+            <p className="section__subtitle--work section__subtitle--work-sub">More undeployed Django projects on my <a target="_blank" href="https://github.com/MohamDah?tab=repositories">GitHub</a></p>
 
             <div className="portfolio">
                 {workEls}
-
-                {/* <a href="https://github.com/me50/MohamDah" className="portfolio__item">
-                    <img src={tba} alt="" className="portfolio__img" />
-                </a> */}
-
             </div>
         </section>
     )
 }
+                // <a href="https://github.com/me50/MohamDah" className="portfolio__item">
+                //     <img src={tba} alt="" className="portfolio__img" />
+                // </a>
