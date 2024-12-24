909b1ebd82ec73bca706208f9e00f594642896f6
MohamDah <mohammedfaiseldhb@gmail.com>
2024-12-24 15:20:58 +0200

    added dark theme

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,5 +1,5 @@
 import logo from "./images/logo.png"
-import React from "react"
+import React, { useEffect, useState } from "react"
 
 export default function Header() {
     const [navOpen, setNavOpen] = React.useState(false);
@@ -8,11 +8,39 @@ export default function Header() {
         setNavOpen(prev => !prev)
     }
 
+    const sysTheme = window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light";
+    const localTheme = localStorage.getItem("theme");
+
+    const [theme, setTheme] = useState(localTheme ? localTheme : sysTheme);
+
+    useEffect(() => {
+        localStorage.setItem("theme", `${theme}`)
+    }, [theme]);
+
+    const root = document.querySelector(":root");
+    root?.setAttribute("color-scheme", `${theme}`);
+
+    function toggleTheme() {
+        setTheme(prev => {
+            return prev === "dark" ? "light" : "dark";
+        })
+    }
+
+
     return (
         <header className={navOpen ? "nav-open" : ""}>
                 <div className="logo">
                     <img src={logo} alt="logo" />
                 </div>
+
+            <button onClick={toggleTheme} className="toggleTheme">
+                {
+                    theme === "light"
+                    ? <i className="fa-regular fa-moon"></i>
+                    : <i className="fa-solid fa-moon"></i>
+                }
+            </button>
+
             <button onClick={toggleNav} className="nav-toggle" aria-label="toggle navigation">
                 <span className="hamburger"></span>
             </button>
