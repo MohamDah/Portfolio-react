f9720424207f2531e20d4e09963325f78f9664c5
MohamDah <mohammedfaiseldhb@gmail.com>
2025-01-09 10:44:02 +0200

    Add new project

--- src/projects.js
+++ src/projects.js
@@ -4,45 +4,53 @@ import vans from "./images/vans.png"
 import rockPaperScissors from "./images/rock-paper-scissors.png"
 import countries from "./images/countries.png"
 import form from "./images/form.png"
+import commentSection from "./images/commentSection.png"
 
 const projects = [
     {
-        id: 1,
+        id: 7,
+        name: "Interactive Comment Section", 
+        image: commentSection,
+        url: "https://mohamdah-interactive-comment-section.netlify.app/",
+        description: ""
+    },
+    {
+        id: 6,
         name: "REST Countries", 
         image: countries,
         url: "https://mohamdah-countries.netlify.app/",
         description: ""
     },
     {
-        id: 2,
+        id: 5,
         name: "Multi-step Form", 
         image: form,
         url: "https://mohamdah-multi-step-form.netlify.app/",
         description: ""
     },
     {
-        id: 3,
+        id: 4,
         name: "Rock Paper Scissors", 
         image: rockPaperScissors,
         url: "https://mohamdah-rock-paper-scissors.netlify.app/",
         description: ""
     },
     {
-        id: 4,
+        id: 3,
         name: "VansLife", 
         image: vans,
         url: "https://mohamdah-vanlife.netlify.app/",
         description: ""
     },
     { 
-        id: 5,
+        id: 2,
         name: "Calculator", 
         image: calc,
         url: "https://mohamdah-calculator.netlify.app/",
         description: "This is my first project to do all by myself after learning React. It's not too complicated, although there were some parts related to logic that were a little annoying to figure out. This is made as a clone of freeCodeCamp's JavaScript Calculator project. I wanted something to practice the React stuff I learnt and thought their project would be great as they would give me a clear target to work toward, instead of trying think of and design an idea of mine. This took me around 7-8 hours of work over the span of 2 days."
     },
     {
-        id: 6,
+        id: 1,
         name: "Tenzies", 
         image: tenzies,
         url: "https://mohamdah-tenzies.netlify.app/",
